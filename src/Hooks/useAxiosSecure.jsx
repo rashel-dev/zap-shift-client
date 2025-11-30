52ff7487f85583a1c10fc7289fe62914e367ceac
rashel9255 <ibrahim2000167@gmail.com>
2025-12-01 00:35:52 +0600

    feat: secured user authentication

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,11 +1,51 @@
-import axios from 'axios';
-import React from 'react';
+import axios from "axios";
+import React from "react";
+import { useEffect } from "react";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router";
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:3000',
-})
+    baseURL: "http://localhost:3000",
+});
 
 const useAxiosSecure = () => {
+    const { user, logOut } = useAuth();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        // Add a request interceptor
+        const requestInterceptor = axiosSecure.interceptors.request.use((config) => {
+            config.headers.Authorization = `Bearer ${user?.accessToken}`;
+
+            return config;
+        });
+
+        //interceptor response
+        const resInterceptor = axiosSecure.interceptors.response.use(
+            (response) => {
+                return response;
+            },
+            (error) => {
+                console.log(error);
+
+                const statusCode = error.status;
+                if (statusCode === 403 || statusCode === 401) {
+                    logOut()
+                    .then(() => {
+                        navigate('/login');
+                    })
+                }
+
+                return Promise.reject(error);
+            }
+        );
+
+        return () => {
+            axiosSecure.interceptors.request.eject(requestInterceptor);
+            axiosSecure.interceptors.response.eject(resInterceptor);
+        };
+    }, [user, logOut, navigate]);
+
     return axiosSecure;
 };
 
